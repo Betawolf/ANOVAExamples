0c8d015572bcd01d504e5613395c9354d571d42c
Matthew Edwards <m.edwards7@lancaster.ac.uk>
2016-02-29 23:55:48 +0000

    Complete LaTeX output
    This version includes a version of workedanova() which produces
    the desired LaTeX output for a given set of data, spelling out
    the computations and generating all tables.

--- examples.r
+++ examples.r
@@ -55,7 +55,7 @@ display3D <- function(data_matrix, alabel="Factor A", blabel="Factor B",
   cat("\\multirow{",a,"}{*}{",alabel,"}")
   for (i in 1:a){
     cat(" & ", i, " & ", paste(lapply(data_matrix[i,], function(x) {
-    paste(round(x,2), collapse=",")}),
+    paste(round(x,2), collapse=", ")}),
     collapse=" & "))
     if (i < a){
       cat("\\\\ \n \\cline{3 -",b+2,"}  \n")
@@ -67,6 +67,28 @@ display3D <- function(data_matrix, alabel="Factor A", blabel="Factor B",
   cat("\\end{tabular}\n\\end{table}\n")
 }
 
+# Return a string showing a sum of squared differences between a vector of values and a single comparison.
+printsquareddiff <- function(vals, mean){
+    return(paste("(", paste(sapply(vals, function(v){ return(paste("(", v, " - ", mean, ")^2", sep="")) }),collapse=" + "), ")"))
+}
+
+# Returns a string showing a particular complex sum.
+printsquaredmultidiff <- function(cel_means, row_means, col_means, sample_mean, r){
+  k <- 0
+  terms <- c()
+  for (i in 1:length(row_means)){
+    for (j in 1:length(col_means)){
+      k <- k + 1
+      terms <- c(terms, paste("(", cel_means[i,j], " - ", row_means[i], " - ", col_means[i], " + ", sample_mean, ")^2", ifelse(k %% 3 == 0, "\\] \n \\[", ""), sep=""))
+    }
+  }
+  termstring <- paste(r, "[", paste(terms, collapse=" + "), "]", sep="")
+  return(paste(termstring, " \\] \n "))
+}
+
+# Prints a LaTeX section showing a worked example of a two-way ANOVA on the 
+# provided 3-dimensional matrix (as structured by `data3d`), spelling out
+# each of the steps in the computation.
 workedanova <- function(data, alabel="Factor A", blabel="Factor B",
     alevels=c(), blevels=c()){
   library(xtable)
@@ -105,6 +127,7 @@ workedanova <- function(data, alabel="Factor A", blabel="Factor B",
 
   SSA <- b * r * sum((row_means - sample_mean) ^ 2)
   SSB <- a * r * sum((col_means - sample_mean) ^ 2)
+  SSABs <- c()
 
   MSA <- SSA/(a-1)
   MSB <- SSB/(b-1)
@@ -115,13 +138,13 @@ workedanova <- function(data, alabel="Factor A", blabel="Factor B",
   
   for (i in 1:a) {
     for (j in 1:b){
-      SSAB <- SSAB + (cel_means[i,j] - row_means[i] - col_means[j] + sample_mean) ^ 2
+      SSABs <- c(SSABs, (cel_means[i,j] - row_means[i] - col_means[j] + sample_mean)) 
       SSerr <- SSerr + sum(sapply(data[i,j], function(ijk) { (ijk - cel_means[i, j]) ^ 2 }))
       SStot <- SStot + sum(sapply(data[i,j], function(ijk) { (ijk - sample_mean) ^ 2 }))
     }
   }
 
-  SSAB <- SSAB * r
+  SSAB <- sum(SSABs ^ 2) * r
   MSAB <- SSAB / ((a - 1)*(b - 1))
 
   MSerr <- SSerr/(length(unlist(data)) - (a*b))
@@ -138,19 +161,28 @@ workedanova <- function(data, alabel="Factor A", blabel="Factor B",
   \\bar{y}_{...})^2 = ", SStot, "\\] \n")
 
   cat("\n \\[ \\mbox{SSA} = br\\sum_{i=1}^a(\\bar{y}_{i..} - \\bar{y}_{...})^2
-  = ", b, "\\times", r, "\\] \n")
+  = ", b, "\\times", r, printsquareddiff(row_means, sample_mean), " = ", b*r, "\\times", sum((row_means - sample_mean) ^ 2), " = ", SSA, "\\] \n")
       
-#  print("Parameters: ")
-#  print(paste("mu hat =",sample_mean))
-#  print("alpha hats: ")
-#  print(row_means - sample_mean)
-#  print("beta hats: ")
-#  print(col_means - sample_mean)
+  cat("\n \\[ \\mbox{SSB} = ar\\sum_{j=1}^b(\\bar{y}_{.j.} - \\bar{y}_{...})^2
+  = ", a, "\\times", r, printsquareddiff(col_means, sample_mean), " = ", a*r, "\\times", sum((col_means - sample_mean) ^ 2), " = ", SSB, "\\] \n")
 
-#  print("alpha/beta hats: ")
-#  for (i in 1:a){
-#    for (j in 1:b){
-#      print(cel_means[i,j] - row_means[i] - col_means[j] + sample_mean)
-#    }
-#  }
+
+  cat("\n \\[ \\mbox{SSAB} = r\\sum_{i=1}^a\\sum_{j=1}^b(\\bar{y}_{ij.} - \\bar{y}_{i..} - \\bar{y}_{.j.} \\bar{y}_{...})^2 = \\] \n \\[")
+  cat(printsquaredmultidiff(cel_means, row_means, col_means, sample_mean, r))
+  cat(paste("\\[ = ",r, "[", paste((SSABs^2), collapse=" + "),"] \\] \n", sep=""), "\\[ =", SSAB, "\\] \n")
+   
+  cat("\\\\ \nParameter estimates are: \\\\ \n")
+  cat("$ \\hat{\\mu } = \\bar{y}_{...} = ", sample_mean, "$ \\\\ \n")
+
+  cat("$ \\hat{\\alpha_{i} } = \\bar{y}_{i..} = ", paste(sapply(row_means, function(i) { paste(i, " - ", sample_mean) }), collapse=","), " = ", paste(row_means - sample_mean, collapse=","), "$ \\\\ \n")
+  cat("$ \\hat{\\beta_{i} } = \\bar{y}_{i..} = ", paste(sapply(col_means, function(i) { paste(i, " - ", sample_mean) }), collapse=","), " = ", paste(col_means - sample_mean, collapse=","), "$ \\\\ \n")
+  cat("$ \\hat{\\alpha\\beta_{ij}} = \\bar{y}_{ij.} - \\bar{y}_{i..} - \\bar{y}_{.j.} \\bar{y}_{...} =", paste(SSABs, collapse=","), "$ \\\\ \n")
+
+  sources <- c("A","B", "A x B", "Error", "Total")
+  degrees <- c(a-1, b-1, ((a-1)*(b-1)), length(unlist(data)) - (a*b), sum(a-1,b-1,((a-1)*(b-1)), length(unlist(data)) - (a*b)))
+  sss <- sapply(c(SSA, SSB, SSAB, SSerr, SStot), function(x){ round(x, 2)})
+  mss <- c(round(MSA,2), round(MSB,2), round(MSAB,2), round(MSerr,2), "")
+  fs <- c(round(MSA/MSerr, 2), round(MSB/MSerr, 2), round(MSAB/MSerr,2), "", "")
+  ft <- data.frame(row.names=sources, DF=degrees, SS=sss, MS=mss, F=fs)
+  xtable(ft)
 }
